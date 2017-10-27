42335b84b4d23a62759970b5a752071e9390636b
David Shaal <david.shaal@gmail.com>
2017-10-27 11:17:27 -0400

    Adding a vanilla_emulsify theme for reference + dist directory in repo

---
+++ web/themes/custom/vanilla_emulsify/gulpfile.js
@@ -0,0 +1,20 @@
+/* globals require */
+
+(function () {
+  'use strict';
+
+  // General
+  var gulp = require('gulp-help')(require('gulp'));
+  var localConfig = {};
+
+  try {
+    localConfig = require('./local.gulp-config');
+  }
+  catch (e) {
+    if (e.code !== 'MODULE_NOT_FOUND') {
+      throw e;
+    }
+  }
+  require('emulsify-gulp')(gulp, localConfig);
+
+})();
